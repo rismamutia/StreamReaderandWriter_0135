aec83a58337ee739e71babe9f334e5ec5b23897c
rismamutia <rismamutiadewi89@gmail.com>
2025-06-10 17:30:02 +0700

    mendapatkan setiap karakter dalam satu baris

--- ioFile.cpp
+++ ioFile.cpp
@@ -11,4 +11,8 @@ int main(){
     outfile.open("contohfile.txt");
     cout << ">= Menulis file, \'q\' untuk keluar" << endl;
     //unlimitted loop untuk menulis
+    while (true) {
+        cout << "- ";
+        //mendapatkan setiap karakter dalam satu baris
+    }
 }
