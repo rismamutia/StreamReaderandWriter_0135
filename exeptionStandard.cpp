5f3a9ffa5308e57289e9f1f6a271090367159a1e
rismamutia <rismamutiadewi89@gmail.com>
2025-06-10 18:36:51 +0700

    penangkap menggunakan obyek exception

--- exeptionStandard.cpp
+++ exeptionStandard.cpp
@@ -14,4 +14,8 @@ int main()
         cout<<data.at(5)<<endl;
         //memanggil array elemen ke 5
     }
+    catch (exception& e) {
+        //penangkap menggunakan obyek exception
+        
+    }
 }
