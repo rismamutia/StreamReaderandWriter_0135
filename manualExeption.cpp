08c62fe1231c63d051df1f8d5a19c6911bee9e6a
rismamutia <rismamutiadewi89@gmail.com>
2025-06-10 18:44:54 +0700

    blok ini akan di eksekusii

--- manualExeption.cpp
+++ manualExeption.cpp
@@ -6,5 +6,10 @@ int main()
     try {
         cout << "Selamat Belajar di Prodi TI UMY" << endl;
         throw 0.5; //melemparkan sebuah integer maka
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
+    }
+    catch (int a) {
+        //blok ini akan di eksekusii
+        cout << "Pengecualian akan dieksekusi" << endl;
     }
 }
