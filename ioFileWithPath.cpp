9e016eed08c64d4f545712152a0847eb864f1181
rismamutia <rismamutiadewi89@gmail.com>
2025-06-10 18:16:59 +0700

    menulis dan memasukkan nilai dari 'baris' ke dalam file

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -17,7 +17,11 @@ int main() {
     while (true) {
         cout << "- ";
         //mendapatkan setiap karakter dalam satu baris
-        
+        getline(cin, baris);
+        //loop akan berhenti jika anda memasukkan karakter q
+        if (baris == "q") break;
+        //menulis dan memasukkan nilai dari 'baris' ke dalam file
+        outfile << baris << endl;
     }
 
 }
