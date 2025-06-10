d74fb6d6a24adafb62c8434d9acb8e814562c7c7
rismamutia <rismamutiadewi89@gmail.com>
2025-06-10 17:31:46 +0700

    //menulis dan memasukkan nilai dari 'baris' ke dalam file

--- ioFile.cpp
+++ ioFile.cpp
@@ -16,5 +16,7 @@ int main(){
         //mendapatkan setiap karakter dalam satu baris
         getline(cin, baris);
         //loop akan berhenti jika anda memasukkan karakter q
+        if (baris == "q") break;
+        //menulis dan memasukkan nilai dari 'baris' ke dalam file
     }
 }
