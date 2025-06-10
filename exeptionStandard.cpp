57ee03e474c74500d743edd018171ac1bf962068
rismamutia <rismamutiadewi89@gmail.com>
2025-06-10 18:32:28 +0700

    penanda 1

--- exeptionStandard.cpp
+++ exeptionStandard.cpp
@@ -2,3 +2,10 @@
 #include <exception>
 //untuk obyek exception yang akan kita gunakan
 #include <array>
+// untuk obyek array yang akan kita gunakan
+using namespace std;
+
+int main()
+{
+    cout << "Awal Program" << endl; //penanda 1:...
+}
