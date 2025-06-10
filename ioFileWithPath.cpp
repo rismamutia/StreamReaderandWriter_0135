d9c43d44f5cff602ee4a2998c1a737dcdef898bd
rismamutia <rismamutiadewi89@gmail.com>
2025-06-10 18:25:30 +0700

    jika file ada, maka akan dilakukan perulangan setiap baris

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -30,5 +30,14 @@ int main() {
     // menunjuk ke sebuah file
     infile.open(NamaFile, ios::in);
     cout << endl << ">= Membuka dan membaca file " << endl;
-    
+    //jika file ada maka
+    if (infile.is_open())
+    {
+        // melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            
+        }
+    }
+
 }
