37ba36a857f5057cad14005d86418ba785111b61
rismamutia <rismamutiadewi89@gmail.com>
2025-06-10 18:10:52 +0700

    membuka file dalam mode menulis

---
+++ ioFileWithPath.cpp
@@ -0,0 +1,13 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string baris;
+    string NamaFile;
+    cout << "Masukkan Nama File : ";
+    cin >> NamaFile;
+    //membuka file dalam mode menulis.
+    ofstream outfile;
+}
